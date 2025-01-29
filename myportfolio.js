0fd4ec696bf2beedb76d4bd7e92ae2d7bdbf5c1e
rishi2785 <rishis2785@gmail.com>
2025-01-29 12:37:10 +0530

    index.html

--- myportfolio.js
+++ myportfolio.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Smooth scrolling for navigation links
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
             e.preventDefault();
